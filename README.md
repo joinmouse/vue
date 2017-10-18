ac63c1b38b7f05ea151a4bd6f56deb71231004f6
wuqi <908380434@qq.com>
2017-10-19 01:01:39 +0800

    vue-cli脚手架搭建

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# vue
+# vue-cloudmusic
 
-> vue-resume-1
+> 基于vue搭建一个移动端音乐的vue项目
 
 ## Build Setup
 
@@ -18,4 +18,4 @@ npm run build
 npm run build --report
 ```
 
-For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Just good</h2>
+    <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br>
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
-    <h2>I love vue</h2>
+    <h2>Ecosystem</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -22,10 +22,10 @@
 
 <script>
 export default {
-  name: 'hello',
+  name: 'HelloWorld',
   data () {
     return {
-      msg: 'I love you'
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
